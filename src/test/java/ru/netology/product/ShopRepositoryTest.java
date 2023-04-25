839167ceff3587a100fe51640206d8351f8c3882
Lesya <lesik_il_89@mail.ru>
2023-04-25 20:45:24 +0300

    Initial commit

---
+++ src/test/java/ru/netology/product/ShopRepositoryTest.java
@@ -0,0 +1,28 @@
+package ru.netology.product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopRepositoryTest {
+
+    @Test
+    public void removeWhenProductExistTest() {
+        ShopRepository shopRepository = new ShopRepository();
+        Product product1 = new Product(1, "Телефон", 30_000);
+        Product product2 = new Product(2, "Ноутбук", 70_000);
+        Product product3 = new Product(3, "Мышка", 800);
+
+        shopRepository.add(product1);
+        shopRepository.add(product2);
+        shopRepository.add(product3);
+
+        shopRepository.remove(1);
+
+        Product[] expected = {product2, product3};
+        Product[] actual = shopRepository.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+}
